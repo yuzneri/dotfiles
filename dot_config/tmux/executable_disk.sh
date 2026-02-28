0ae219fb9335b1bf330193ac0a3d9acb41931de7
YuzuneYuzuriha <git@yuzneri.net>
2026-02-28 23:42:22 +0900

    del

--- dot_config/tmux/executable_disk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-pct=$(df -h / | awk 'NR==2{gsub(/%/,"",$5); printf "%.0f",$5}')
-tmux set -gq @disk_pct "$pct"
-printf '%2d' "$pct"
--- dot_config/tmux/executable_mem.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-pct=$(awk '/^MemTotal:/{t=$2} /^MemAvailable:/{a=$2} END{printf "%.0f",(t-a)/t*100}' /proc/meminfo)
-tmux set -gq @mem_pct "$pct"
-printf '%2d' "$pct"
